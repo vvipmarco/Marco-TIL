f5c6f80dac60bf1ad29d64b5d9b416b1fa4ed3da
vvipmarco <155531611+vvipmarco@users.noreply.github.com>
2024-03-27 23:58:49 +0900

    [level 0] Title: 조건 문자열, Time: 0.00 ms, Memory: 10 MB -BaekjoonHub

---
+++ 프로그래머스/0/181934. 조건 문자열/조건 문자열.py
@@ -0,0 +1,11 @@
+def solution(ineq, eq, n, m):
+    answer = 0
+    if ineq == ">" and eq == "=":
+        answer = int(n >= m)
+    elif ineq == "<" and eq == "=":
+        answer = int(n <= m)
+    elif ineq == ">" and eq == "!":
+        answer = int(n > m)
+    elif ineq == "<" and eq == "!":
+        answer = int(n < m)
+    return answer
